5bfde256159de88700a314d79c6661e403e65a39
spacia <spacia@objectivity.co.uk>
2015-11-16 12:07:47 +0100

    added task2 tests

--- Objectivity.Test.Automation.Light.Aad/Pages/Task2.cs
+++ Objectivity.Test.Automation.Light.Aad/Pages/Task2.cs
@@ -5,13 +5,62 @@ using System.Text;
 using System.Threading.Tasks;
 using Objectivity.Test.Automation.Light.Common;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+
 
 namespace Objectivity.Test.Automation.Light.Aad.Pages
 {
     public class Task2 : PageBase
     {
+        private By selectLocator = By.CssSelector("select");
+
+        [FindsBy(How = How.CssSelector, Using = "span.select2-selection")]
+        private IWebElement WybierzProduktList;
+
+        [FindsBy(How = How.CssSelector, Using = "input.select2-search__field")]
+        private IWebElement WybierzProduktWSearchInput;
+
+        [FindsBy(How = How.CssSelector, Using = "div.caption")]
+        private IList<IWebElement> ProductsList;
+
         public Task2(IWebDriver driver) : base(driver)
         {
+        }
+
+        public void ClickWybierzProdukList()
+        {
+            WybierzProduktList.Click();
+        }
+        public bool IsWybierzProductExpanded()
+        {
+            var expanded = WybierzProduktList.GetAttribute("aria-expanded").Contains("true");
+            return expanded;
+        }
+
+        public IList<IWebElement> GetProductList()
+        {
+            var select = new SelectElement(Driver.FindElement(selectLocator));
+            var values = select.Options.ToList();
+            return values;
+        }
+
+        public void SearchWybierzProduktList(string text)
+        {
+            ClickWybierzProdukList();
+            WybierzProduktWSearchInput.SendKeys(text);
+            WybierzProduktWSearchInput.SendKeys(Keys.Enter);
+        }
+
+        public string GetSelectedWybierzProductList()
+        {
+            var selected = WybierzProduktList.Text;
+            return selected;
+        }
+
+        public IList<IWebElement> GetProducts()
+        {
+            return ProductsList.ToList();
         }
     }
 }
--- Objectivity.Test.Automation.Light.Aad/Tests/Zadanie2/Zadanie2.cs
+++ Objectivity.Test.Automation.Light.Aad/Tests/Zadanie2/Zadanie2.cs
@@ -4,30 +4,74 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
-namespace Objectivity.Test.Automation.Light.Aad.Tests.JavaScriptAlertsTests
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using NUnit.Framework;
+using Objectivity.Test.Automation.Light.Aad.Pages;
+using Objectivity.Test.Automation.Light.Common;
+using OpenQA.Selenium;
+
+namespace Objectivity.Test.Automation.Light.Aad.Tests.Zadanie2
 {
-    using NUnit.Framework;
-
-    using Objectivity.Test.Automation.Light.Aad.Pages;
-    using Objectivity.Test.Automation.Light.Common;
-
-    /// <summary>
-    /// The java script alerts tests.
-    /// </summary>
+    [Category("Zadanie2")]
     public class Zadanie2 : TestBase
     {
-        /// <summary>
-        /// The confirm java script alert test.
-        /// </summary>
-        [Test]
-        public void ConfirmJavaScriptAlertTest()
+        [SetUp]
+        public void OpenTaskPage()
         {
-            var examplePage = new ExamplePage(this.Driver);
-            var javaScriptAlertsPage = examplePage.GoToJavaScriptAlerts();
-            javaScriptAlertsPage.OpenJsAlert();
-            this.Driver.SwitchTo().Alert().Accept();
-            this.Driver.SwitchTo().DefaultContent();
-            Assert.True(javaScriptAlertsPage.Contains("You successfuly clicked an alert"));
+            Driver.Navigate().GoToUrl(GetHost() + "task_2");
+        }
+
+        [Test]
+        public void VerifyCategoriesAfterClickTest()
+        {
+            var taskPage = new Task2(this.Driver);
+            taskPage.ClickWybierzProdukList();
+            Assert.True(taskPage.IsWybierzProductExpanded());
+        }
+
+        [Test]
+        public void VerifyCategoriesListTest()
+        {
+            var list = new List<string> {"-1", "Sport", "Elektronika", "Firma i usługi"};
+            var taskPage = new Task2(this.Driver);
+            var categoryList = taskPage.GetProductList();
+            for (int i = 0; i < list.Count; i++)
+            {
+                Assert.AreEqual(list[i], categoryList[i].GetAttribute("value"));
+            }
+        }
+
+        [Test]
+        public void DefaultWybierzProduktListValueTest()
+        {
+            var taskPage = new Task2(this.Driver);
+            Assert.AreEqual("Proszę wybrać kategorie", taskPage.GetSelectedWybierzProductList());
+        }
+
+        [Test]
+        public void FilterElektronikaTest()
+        {
+            var taskPage = new Task2(this.Driver);
+            taskPage.SearchWybierzProduktList("Elektronika");
+            var products = taskPage.GetProducts();
+            foreach (var product in products)
+            {
+                Assert.AreEqual("Elektronika", product.FindElement(By.TagName("strong")).Text);
+            }
+        }
+
+        [Test]
+        public void FilterFirmaIUslugiTest()
+        {
+            var taskPage = new Task2(this.Driver);
+            taskPage.SearchWybierzProduktList("uslugi");
+            var products = taskPage.GetProducts();
+            foreach (var product in products)
+            {
+                Assert.AreEqual("Firma i usługi", product.FindElement(By.TagName("strong")).Text);
+            }
         }
     }
 }

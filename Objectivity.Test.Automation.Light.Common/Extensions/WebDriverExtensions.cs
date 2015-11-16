3105d8690b7e446157bca2fa6e209f695fbf3916
spacia <spacia@objectivity.co.uk>
2015-11-16 08:15:40 +0100

    Added WebDriverExtensions

---
+++ Objectivity.Test.Automation.Light.Common/Extensions/WebDriverExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace Objectivity.Test.Automation.Light.Common.Extensions
+{
+    public static class WebDriverExtensions
+    {
+        public static IWebElement WaitForElement(this IWebDriver webDriver, By locator, double timeoutInSeconds)
+        {
+            IWebElement element;
+            try
+            {
+                var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(timeoutInSeconds));
+                element = wait.Until(ExpectedConditions.ElementIsVisible(locator));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            return null;
+        }
+    }
+}
